a8bb63fe69de2684fd5978ce537d6027f2df0032
sean <spmcdonald1316@gmail.com>
2026-01-31 22:07:05 -0600

    create guesstheflag, add assets, begin layout

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sean McDonald on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
