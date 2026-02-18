6b3d7a7d8c92f2b9c695f8902cdccab1093b8b3b
sean <spmcdonald1316@gmail.com>
2026-02-18 09:59:44 -0600

    add basic layout

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,15 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                Section {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    DatePicker(
+                        "Please enter a time",
+                        selection: $wakeUp,
+                        displayedComponents: .hourAndMinute
+                    )
+                    .labelsHidden()
+                    .padding(EdgeInsets(top: 5, leading: 0, bottom: 10, trailing: 0))
+                }
+                Spacer()
+                Section {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                        .padding(EdgeInsets(top: 5, leading: 100, bottom: 5, trailing: 100))
+                }
+                Spacer()
+                Section {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+                        .padding(EdgeInsets(top: 5, leading: 100, bottom: 5, trailing: 100))
+                }
+                Spacer()
+                Spacer()
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
         }
-        .padding()
     }
+    
+    func calculateBedtime() {}
 }
 
 #Preview {
