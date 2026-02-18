034b273e5580cf465a068a999ab1862938e7a309
sean <spmcdonald1316@gmail.com>
2026-02-18 09:25:25 -0600

    create new project

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sean McDonald on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
