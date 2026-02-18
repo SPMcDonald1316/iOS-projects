166891c74842147fc8992e8d545e069d4f8f675f
sean <spmcdonald1316@gmail.com>
2026-02-18 16:21:06 -0600

    use CoreML to calculate needed sleep

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Sean McDonald on 2/18/26.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -53,7 +54,24 @@ struct ContentView: View {
         }
     }
     
-    func calculateBedtime() {}
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents(
+                [.hour, .minute],
+                from: wakeUp
+            )
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(
+                hour + minute
+            ), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+        } catch {}
+    }
 }
 
 #Preview {
