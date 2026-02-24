5441c2c0ec799a5d508bc4a312e15aa157e16689
sean <spmcdonald1316@gmail.com>
2026-02-24 16:48:13 -0600

    create new project, setup layout, initial vars

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Sean McDonald on 2/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle(rootWord)
+        .onSubmit(addNewWord)
+    }
+    
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicates with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // exit if the remaining string is empty
+        guard answer.count > 0 else { return }
+        
+        // extra valiation to come
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+}
+
+#Preview {
+    ContentView()
+}
