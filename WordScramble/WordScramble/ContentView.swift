085e5c06ccb98d19052625bcf57807637b507e8b
sean <spmcdonald1316@gmail.com>
2026-02-26 12:21:10 -0600

    complete challenges

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var showingError = false
+    @State private var score = 0
     
     var body: some View {
         NavigationStack {
@@ -31,12 +32,25 @@ struct ContentView: View {
                         }
                     }
                 }
+                
+                
+            
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
             .onAppear(perform: startGame)
             .alert(errorTitle, isPresented: $showingError) { } message: {
                 Text(errorMessage)
+            }
+            .toolbar {
+                Button("Start New Game") {
+                    startGame()
+                }
+            }
+            
+            Section {
+                Text("Score: \(score)")
+                    .font(.largeTitle.bold())
             }
         }
     }
@@ -47,6 +61,12 @@ struct ContentView: View {
         
         // exit if the remaining string is empty
         guard answer.count > 0 else { return }
+        
+        // no words shorter than 3 letters
+        guard answer.count > 2 else {
+            wordError(title: "Word too short", message: "Words must be at least 3 letters long.")
+            return
+        }
         
         guard isOriginal(word: answer) else {
             wordError(title: "Word used already", message: "Be more original")
@@ -66,10 +86,20 @@ struct ContentView: View {
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
+        
+        switch answer.count {
+            case 3...4: score += 1
+            case 5...6: score += 2
+            case 7...8: score += 3
+            default: break
+        }
         newWord = ""
     }
     
     func startGame() {
+        // clear usedWords on restart
+        usedWords.removeAll()
+        
         // Find the URL for start.txt in our app bundle
         if let startWordsURL = Bundle.main.url(
             forResource: "start",
@@ -114,7 +144,7 @@ struct ContentView: View {
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = check.rangeOfMisspelledWord(
+        let misspelledRange = checker.rangeOfMisspelledWord(
             in: word,
             range: range,
             startingAt: 0,
