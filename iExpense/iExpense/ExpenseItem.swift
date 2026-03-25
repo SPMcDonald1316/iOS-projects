c101de5cc8288b58578e2e6ffc37a81340178e4c
sean <spmcdonald1316@gmail.com>
2026-03-25 16:17:54 -0500

    create needed data structures, begin laying out app

---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,12 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Sean McDonald on 3/25/26.
+//
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
---
+++ iExpense/iExpense/Expenses.swift
@@ -0,0 +1,13 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Sean McDonald on 3/25/26.
+//
+
+import SwiftUI
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
