bc71138b42a28a30ebbaeb3db2d8784f81a9d155
sean <spmcdonald1316@gmail.com>
2026-04-02 16:35:54 -0500

    initial project commit

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Sean McDonald on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
