c5f2d8dfe803ef83104d2d2c3971d4dd73d6038d
sean <spmcdonald1316@gmail.com>
2026-04-02 17:06:56 -0500

    add assets, create Astronaut struct

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Sean McDonald on 4/2/26.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
