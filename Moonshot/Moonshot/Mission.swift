4329165eeedce96cd9189951f64c4e3ea075ee8c
sean <spmcdonald1316@gmail.com>
2026-04-03 16:12:31 -0500

    add structs for mission data

---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Sean McDonald on 4/3/26.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
