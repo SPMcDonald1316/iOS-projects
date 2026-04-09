4354188b8df3e3c8a82a0105c67cf55397be018b
sean <spmcdonald1316@gmail.com>
2026-04-09 13:04:19 -0500

    create grid and list layouts add functionality to toggle between them

---
+++ Moonshot/Moonshot/ListLayout.swift
@@ -0,0 +1,50 @@
+//
+//  ListLayout.swift
+//  Moonshot
+//
+//  Created by Sean McDonald on 4/9/26.
+//
+
+import SwiftUI
+
+struct ListLayout: View {
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
+    
+    var body: some View {
+        List {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: astronauts)
+                } label: {
+                    HStack {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .padding()
+                        
+                        VStack {
+                            Text(mission.displayName)
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            Text(mission.formattedLaunchDate)
+                                .font(.caption)
+                                .foregroundStyle(.white.opacity(0.5))
+                        }
+                        .padding(.vertical)
+                        .frame(maxWidth: .infinity)
+                        .background(.lightBackground)
+                    }
+                }
+                .listRowBackground(Color.darkBackground)
+                .listRowSeparator(.hidden)
+            }
+        }
+        .listStyle(.plain)
+    }
+}
+
+#Preview {
+//    ListLayout()
+}
