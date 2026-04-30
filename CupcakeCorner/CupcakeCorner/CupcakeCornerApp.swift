428de2bf29bac014a45477ffcbea910b2e913625
sean <spmcdonald1316@gmail.com>
2026-04-30 15:57:56 -0500

    begin new project

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Sean McDonald on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
