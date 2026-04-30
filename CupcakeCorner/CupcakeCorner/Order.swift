b9dc778879aeaff90b2972e194af3ad6f9a64b8c
sean <spmcdonald1316@gmail.com>
2026-04-30 16:11:22 -0500

    create order model

---
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,20 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Sean McDonald on 4/30/26.
+//
+
+import Foundation
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false
+    var extraFrosting = false
+    var addSprinkles = false
+}
