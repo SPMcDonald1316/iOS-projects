3825b6bd601619f85cd491f6326aabaea2e0b01f
sean <spmcdonald1316@gmail.com>
2026-05-05 16:34:08 -0500

    complete first screen, add dummy view for nav purposes

---
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Sean McDonald on 5/5/26.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
