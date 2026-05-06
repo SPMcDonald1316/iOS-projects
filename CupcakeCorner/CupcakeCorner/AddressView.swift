63fd81289a0704a683d074e2cd8bac12c3fcc3bb
sean <spmcdonald1316@gmail.com>
2026-05-06 10:55:12 -0500

    add address validation to order object, add disabled modifier to address view

--- CupcakeCorner/CupcakeCorner/AddressView.swift
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -24,6 +24,7 @@ struct AddressView: View {
                     CheckoutView(order: order)
                 }
             }
+            .disabled(order.hasValidAdress == false)
         }
         .navigationTitle("Delivery details")
         .navigationBarTitleDisplayMode(.inline)
