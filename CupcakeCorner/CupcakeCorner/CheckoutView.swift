04580923f0391aacd7fe62e3bb709ca8fca1fa82
sean <spmcdonald1316@gmail.com>
2026-05-06 17:17:50 -0500

    add place order async function to encode and send order data

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    @State private var confimationMessage = ""
+    @State private var showingConfirmation = false
     var order: Order
     
     var body: some View {
@@ -25,13 +27,44 @@ struct CheckoutView: View {
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Place Order", action: {})
+                Button("Place Order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
                     .padding()
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") {}
+        } message: {
+            Text(confimationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            print(String(data: data, encoding: .utf8) ?? "Invalid response")
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confimationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed: \(error.localizedDescription)")
+        }
     }
 }
 
