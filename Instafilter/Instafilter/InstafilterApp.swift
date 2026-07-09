c9ab9ec95780b1291016716c7752b4fcf94dec36
sean <spmcdonald1316@gmail.com>
2026-07-09 15:42:15 -0500

    begin new project

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Sean McDonald on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
