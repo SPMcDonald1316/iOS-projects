06dc61fca3a5886941f1acffb8b559ebe145e33b
sean <spmcdonald1316@gmail.com>
2026-07-10 15:56:50 -0500

    wrap image area in PhotosPicker

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,27 +5,35 @@
 //  Created by Sean McDonald on 7/7/26.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
         NavigationStack {
             VStack {
                 Spacer()
                 
-                if let processedImage {
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView(
-                        "No Picture",
-                        systemImage: "photo.badge.plus",
-                        description: Text("Tap to import a photo")
-                    )
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView(
+                            "No Picture",
+                            systemImage: "photo.badge.plus",
+                            description: Text("Tap to import a photo")
+                        )
+                    }
                 }
                 
                 Spacer()
