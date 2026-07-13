fa7a1a9b7829cb5bf64d62b46f12429f09a6a8e5
sean <spmcdonald1316@gmail.com>
2026-07-13 15:30:27 -0500

    add share link

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -52,7 +52,12 @@ struct ContentView: View {
                     
                     Spacer()
                     
-                    // share the picture
+                    if let processedImage {
+                        ShareLink(
+                            item: processedImage,
+                            preview: SharePreview("Instafilter image", image: processedImage)
+                        )
+                    }
                 }
             }
             .padding([.horizontal, .bottom])
