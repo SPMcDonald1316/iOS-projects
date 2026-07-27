1ceed94dbacb5e81b8d195208ceb3ce902a1a862
sean <spmcdonald1316@gmail.com>
2026-07-27 17:22:15 -0500

    add Comparable protocl to Page struct, compute description string, complete EditView

--- BucketList/BucketList/EditView.swift
+++ BucketList/BucketList/EditView.swift
@@ -37,7 +37,7 @@ struct EditView: View {
                                     Text(page.title)
                                         .font(.headline)
                                     Text(": ")
-                                    Text("Page description here")
+                                    Text(page.description)
                                         .italic()
                                 }
                             }
@@ -87,7 +87,7 @@ struct EditView: View {
             
             let items = try JSONDecoder().decode(Result.self, from: data)
             
-            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            pages = items.query.pages.values.sorted()
             loadingState = .loaded
         } catch {
             loadingState = .failed
