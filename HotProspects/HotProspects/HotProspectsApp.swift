cb740c0a67cef7b0e3c3614bb39a25f7dd7b2d66
sean <spmcdonald1316@gmail.com>
2026-08-03 16:50:03 -0500

    begin new project

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Sean McDonald on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
