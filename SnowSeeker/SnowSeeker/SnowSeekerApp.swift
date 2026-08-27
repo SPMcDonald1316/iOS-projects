49e61898fd220fdcaf871b1183b23ee340f5f9ea
sean <spmcdonald1316@gmail.com>
2026-08-27 12:13:34 -0500

    Begin new project

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Sean McDonald on 8/27/26.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
